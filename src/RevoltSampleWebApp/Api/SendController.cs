04f1aa20299910baef549cb8f612b74b2ba826af
FlexCast <public@flex-cast.cz>
2020-01-27 09:18:28 +0100

    8) Emails - not yet working, just code template

---
+++ src/RevoltSampleWebApp/Api/SendController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RevoltSampleWebApp.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SendController : ControllerBase
+    {
+        private readonly UserManager<Models.ApplicationUser> userManager;
+        private readonly Models.EmailFactory emailFactory;
+
+        public SendController(UserManager<Models.ApplicationUser> userManager)
+        {
+            this.userManager = userManager;
+            this.emailFactory = new Models.EmailFactory();
+        }
+        // GET: api/Send
+        [HttpGet]
+        public bool Get()
+        {
+            var users = userManager.Users;
+            foreach (var user in users)
+            {
+                SendEmail(user);
+            }
+            return true;
+        }
+
+        void SendEmail(Models.ApplicationUser user)
+        {
+            string emailAddress = user.Email;
+            emailFactory.SendEmail(user.Email, user.ID1, user.ID2);
+        }
+    }
+}
---
+++ src/RevoltSampleWebApp/Models/EmailFactory.cs
@@ -0,0 +1,38 @@
+﻿using System.Net.Mail;
+
+
+namespace RevoltSampleWebApp.Models
+{
+    public class EmailFactory
+    {
+
+        SmtpClient smtpClient;
+
+        public EmailFactory()
+        {
+            smtpClient = new SmtpClient("smtp.forpsi.com", 465);
+
+            smtpClient.Credentials = new System.Net.NetworkCredential("revolt_test@flex-cast.cz", "Revolt123");
+            smtpClient.UseDefaultCredentials = false;
+            smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
+            smtpClient.EnableSsl = true;
+        }
+
+        private string CreateHtmlMessage(string ID1, string ID2)
+        {
+            return "<a href=\"https://localhost/newpage/" + ID1 + "/" + ID2 + "\" >LINK</a>" ;
+        }
+
+        public void SendEmail(string recipeint, string ID1, string ID2)
+        {
+            MailMessage mail = new MailMessage();
+            mail.From = new MailAddress("revolt_test@flex-cast.cz", "RevoltTestWebsiteMailBot");
+            mail.To.Add(new MailAddress(recipeint));
+            mail.Subject = "RevoltTestEmail";
+            mail.Body = CreateHtmlMessage(ID1, ID2);
+
+            smtpClient.Send(mail);
+        }
+
+    }
+}
