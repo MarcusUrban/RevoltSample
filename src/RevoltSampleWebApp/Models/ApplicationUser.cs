8e2d3c5d572f339751304def0d048ddca215b02d
FlexCast <public@flex-cast.cz>
2020-01-27 08:08:40 +0100

    4) Extending IdentityUser for properties ID1, ID2

---
+++ src/RevoltSampleWebApp/Models/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+
+namespace RevoltSampleWebApp.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string ID1 { get; set; }
+        public string ID2 { get; set; }
+    }
+}
