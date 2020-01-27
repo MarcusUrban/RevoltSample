4337b62f14c865df5b9690e149e19060b8f104df
FlexCast <public@flex-cast.cz>
2020-01-27 11:59:49 +0100

    2) Adding default application users

--- src/RevoltSampleWebApp/Data/ApplicationDbContext.cs
+++ src/RevoltSampleWebApp/Data/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -13,6 +14,44 @@ namespace RevoltSampleWebApp.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            var adminUser = new Models.ApplicationUser
+            {
+                Id = Guid.NewGuid().ToString(),
+                UserName = "Admin",
+                NormalizedUserName = "Admin".ToUpper(),
+                Email = "admin@testing.cz",
+                NormalizedEmail = "admin@testing.cz".ToUpper(),
+                EmailConfirmed = true,
+                SecurityStamp = Guid.NewGuid().ToString()
+
+            };
+
+            var managerUser = new Models.ApplicationUser
+            {
+                Id = Guid.NewGuid().ToString(),
+                UserName = "Manager",
+                NormalizedUserName = "Manager".ToUpper(),
+                Email = "manager@testing.cz",
+                NormalizedEmail = "manager@testing.cz".ToUpper(),
+                EmailConfirmed = true,
+                SecurityStamp = Guid.NewGuid().ToString()
+            };
+
+            var hashGenerator = new PasswordHasher<Models.ApplicationUser>();
+            adminUser.PasswordHash = hashGenerator.HashPassword(adminUser, "Admin12.");
+            managerUser.PasswordHash = hashGenerator.HashPassword(managerUser, "Manager12.");
+
+            builder.Entity<Models.ApplicationUser>().HasData(
+                    adminUser,
+                    managerUser
+                );
+
         }
     }
 }
---
+++ src/RevoltSampleWebApp/Data/Migrations/20200127105831_SeedUserData.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RevoltSampleWebApp.Data.Migrations
+{
+    public partial class SeedUserData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetUsers",
+                columns: new[] { "Id", "AccessFailedCount", "ConcurrencyStamp", "Email", "EmailConfirmed", "ID1", "ID2", "LockoutEnabled", "LockoutEnd", "NormalizedEmail", "NormalizedUserName", "PasswordHash", "PhoneNumber", "PhoneNumberConfirmed", "SecurityStamp", "TwoFactorEnabled", "UserName" },
+                values: new object[] { "6c80fe71-55df-4177-960d-82ad984e4824", 0, "a264943e-6008-4884-ae60-9c92ea27cae6", "admin@testing.cz", true, null, null, false, null, "ADMIN@TESTING.CZ", "ADMIN", "AQAAAAEAACcQAAAAEDDiGOGtRm29t7eQm9ezhMbNkasl3zs3nBLmsQroAKapXMh1Kp/9J2jqqTaLAWr2bA==", null, false, "19f34311-8c8a-4581-b337-5cafc7cb3b53", false, "Admin" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUsers",
+                columns: new[] { "Id", "AccessFailedCount", "ConcurrencyStamp", "Email", "EmailConfirmed", "ID1", "ID2", "LockoutEnabled", "LockoutEnd", "NormalizedEmail", "NormalizedUserName", "PasswordHash", "PhoneNumber", "PhoneNumberConfirmed", "SecurityStamp", "TwoFactorEnabled", "UserName" },
+                values: new object[] { "22073247-a532-496f-9785-abfc6912bcf1", 0, "31066525-b921-4503-adb3-3604568cffd0", "manager@testing.cz", true, null, null, false, null, "MANAGER@TESTING.CZ", "MANAGER", "AQAAAAEAACcQAAAAEGUnea0vUvwu4z9shavylr6IglOOf8IowY4FeZ9j50MoJeJYOpRgjBDZpRTpT1sZAA==", null, false, "c9729723-904b-4546-b1d4-226736b9fd75", false, "Manager" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "22073247-a532-496f-9785-abfc6912bcf1");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "6c80fe71-55df-4177-960d-82ad984e4824");
+        }
+    }
+}
