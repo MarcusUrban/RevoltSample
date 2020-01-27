a323c332e25e8139511d42ba0774713a92ce9c54
FlexCast <public@flex-cast.cz>
2020-01-27 08:28:09 +0100

    7) Adding api/Generate
    + RandomWordFactory class - gets random word from REST API
    + GenerateController - handles api/Generate calls
    + Adding routing and service registration for controllers in Razor pages

---
+++ src/RevoltSampleWebApp/Api/GenerateController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RevoltSampleWebApp.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GenerateController : ControllerBase
+    {
+        private readonly UserManager<Models.ApplicationUser> userManager;
+        private readonly Models.RandomWordFactory randomWordFactory;
+
+        public GenerateController(UserManager<Models.ApplicationUser> userManager)
+        {
+            this.userManager = userManager;
+            this.randomWordFactory = new Models.RandomWordFactory();
+
+        }
+        // GET: api/Generate
+        [HttpGet]
+        public async Task<bool> Get()
+        {
+            IList<Models.ApplicationUser> users = userManager.Users.ToList();
+            randomWordFactory.GenerateWords((uint)users.Count * (uint)2);
+
+            foreach (var user in users)
+            {
+                user.ID1 = randomWordFactory.Words.Pop();
+                user.ID2 = randomWordFactory.Words.Pop();
+                await userManager.UpdateAsync(user);
+            }
+
+            return true;
+        }
+    }
+}
---
+++ src/RevoltSampleWebApp/Models/RandomWordFactory.cs
@@ -0,0 +1,32 @@
+﻿using System.Net;
+using System.Collections.Generic;
+
+namespace RevoltSampleWebApp.Models
+{
+    public class RandomWordFactory
+    {
+        public Stack<string> Words { private set; get; }
+
+        public RandomWordFactory()
+        {
+            Words = new Stack<string>();
+        }
+        public void GenerateWords(uint count)
+        {
+            WebClient client = new WebClient();
+            string rawJSON = client.DownloadString("https://random-word-api.herokuapp.com/word?key=8E2EZH99&number=" + count.ToString());
+            // Clearing JSON file
+            rawJSON = rawJSON.Trim('[', ']');
+            // Parsing JSON file
+            string[] wordsRaw = rawJSON.Split(',');
+
+            Words.Clear();
+            foreach (string word in wordsRaw)
+            {
+                // Removing quotes from individual words
+                Words.Push(word.Substring(2, word.Length - 3));
+            }
+
+        }
+    }
+}
